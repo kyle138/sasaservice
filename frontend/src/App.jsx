d2c111806e8a1ce75dfbd57194042fa7cd98743c
Kyle Munz <kyle138@gmail.com>
2026-07-04 17:45:10 -0500

    About placeholder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
 import Home from './pages/Home';
+import About from './pages/About';
 import Contact from './pages/Contact';
 import './App.css';
 
@@ -9,12 +10,14 @@ function App() {
       {/* Navigation */}
       <nav>
         <NavLink to="/">Home</NavLink> |{" "}
+        <NavLink to="/about">API</NavLink>
         <NavLink to="/contact">Contact</NavLink>
       </nav>
 
       {/* Routes */}
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>Make look pretty later...</p>
+    </>
+  )
+};
+
+export default About;

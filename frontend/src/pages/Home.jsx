353456aa8cef24e777b3ec3ebda82d2eee98f37c
Kyle Munz <kyle138@gmail.com>
2026-07-31 10:33:29 -0500

    Placeholder for api/about

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -129,7 +129,7 @@ function Home() {
             </Col>
           </Form.Group>
                 </Row>
-          <Row>
+          <Row className="ps-5">
             <Col>{response}</Col>
           </Row>
         </>

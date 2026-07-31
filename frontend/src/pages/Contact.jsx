61d18cc36e45b5925815b42787a7d12301573084
Kyle Munz <kyle138@gmail.com>
2026-07-31 10:45:10 -0500

    placeholder for Contact

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import CoolS from '../assets/CoolS.svg';
+import Logo from '../assets/SasaService_logo_DS.svg';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Tooltip from 'react-bootstrap/Tooltip';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 
 function Contact() {
   return (
-    <>
-      <h1>Contact</h1>
-      <p>Make look pretty later...</p>
-    </>
+    <Container className='vt323-regular crt mb-5'>
+      <Row className='pt-4 pb-2'>
+        <OverlayTrigger
+          delay={{ show: 138, hide: 400 }}
+          overlay={<Tooltip id="LogoTip">'S' as a Service, redefining Saas!</Tooltip>}
+        >
+          <Image
+            id="logo"
+            src={Logo}
+            alt="'S' as a Service, redefining Saas!"
+          />
+        </OverlayTrigger>
+      </Row>
+      <Row className='py-2 ps-5 text-center'>
+        <h1>Contact</h1>
+        <p>Make look pretty later...</p>
+      </Row>
+      <Row className='pt-4 pb-3 pe-5'>
+        <Col>
+          <p className='text-end'>© 2026</p>
+        </Col>
+      </Row>
+    </Container>
   )
 };
 
